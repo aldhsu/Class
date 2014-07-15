877b2dece2acfb47d51a50a0f510c21366b5f430
Allen <0allen.hsu@gmail.com>
2014-07-15 11:11:54 +1000

    40

--- 40/series_mult.rb
+++ 40/series_mult.rb
@@ -1,9 +1,9 @@
 class Series
-  def self.find_largest_consecutive(series, length)
-    total_series = series.length - length
+  def self.find_largest_consecutive(series, elem)
+    total_series = series.length - elem
     current_max = 0
     (0..total_series).each do |position|
-      numbers = series[position, length]
+      numbers = series[position, elem]
       mult = numbers.chars.inject(1){|memo, num| memo * num.to_i}
       current_max = mult if mult > current_max
     end
